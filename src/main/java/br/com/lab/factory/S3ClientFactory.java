dc1421f631959af966e5e57af353380d7070b762
Everton Rocha <imevertonrch@gmail.com>
2024-12-23 23:53:08 -0300

    refactory: removendo redundância

--- src/main/java/br/com/lab/factory/S3ClientFactory.java
+++ src/main/java/br/com/lab/factory/S3ClientFactory.java
@@ -6,27 +6,35 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
+
+/**
+ * Por padrão o client do S3 é imutável, dessa forma foi necessário criar
+ * uma factory.
+ */
+
 @Component
 public class S3ClientFactory {
 
     private final AWSStaticCredentialsProvider provider;
-    private AmazonS3 client;
 
     public S3ClientFactory(AWSStaticCredentialsProvider provider) {
         this.provider = provider;
     }
 
-    // O client do S3 é imutável por padrão, foi criado uma factory para ter a flexibilidade
-    // de alterar a região
+    /**
+     * Cria um cliente S3 com base na região especificada.
+     *
+     * @param region Região desejada. Caso seja nulo, será usada a região padrão.
+     * @return Instância de AmazonS3.
+     */
     public AmazonS3 createClient(Regions region) {
+        Regions regiaoEfetiva = (region != null) ? region : Regions.DEFAULT_REGION;
+
         return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+                .withRegion(regiaoEfetiva)
                 .withCredentials(provider)
                 .build();
-
-    }
-
-    public AmazonS3 getClient() {
-        return this.client;
     }
 }
